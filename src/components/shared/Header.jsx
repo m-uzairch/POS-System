a3464afff4719617944a52c1447cd68611baa0b8
Uzair Ch <uzairchaudhary235@gmail.com>
2025-11-06 00:40:06 +0500

    Images

---
+++ src/components/shared/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FaSearch } from 'react-icons/fa';
+import { FaUserCircle } from 'react-icons/fa';
+import { FaBell } from 'react-icons/fa';
+import logo from "../../../assets/logo.png";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center py-4 px-8 bh-[#1a1a1a]">
+      {/* Logo */}
+      <div>
+        <img src="" alt="" />
+      </div>
+    </header>
+  )
+}
+
+export default Header
