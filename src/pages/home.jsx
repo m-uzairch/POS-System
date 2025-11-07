08c5dfdb5595fae6089df1edb7a42f7f96f5a9f2
Uzair Ch <uzairchaudhary235@gmail.com>
2025-11-07 10:01:42 +0500

    Date Time

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
-import BottomNav from '../components/shared/BottomNav'
+import React from "react";
+import BottomNav from "../components/shared/BottomNav";
 
 function home() {
   return (
-    <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3'>
+    <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
       {/* Left Div */}
-      <div className='flex-[3] bg-[#ff0000]'>
-
-      </div>
+      <div className="flex-3 bg-[#ff0000]"></div>
       {/* Right Div */}
-      <div className='flex-[2] bg-[#0015ff]'>
-
-      </div>
+      <div className="flex-2 bg-[#0015ff]"></div>
       <BottomNav />
     </section>
-  )
+  );
 }
 
-export default home
+export default home;
